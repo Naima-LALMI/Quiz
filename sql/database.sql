e8251cecd0c8737b1fc52f361ce2ffc7c076297b
Naima <naima.lalmi28@gmail.com>
2024-10-23 12:15:40 +0200

    creation 3 tables : question, utilisateur, questions_utilisateur

---
+++ sql/database.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE quiz;
+CREATE TABLE utilisateur (
+id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100),
+        prenom VARCHAR(100),
+email VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE questions (
+id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,    question TEXT NOT NULL,
+    option_a VARCHAR(255) NOT NULL,
+    option_b VARCHAR(255) NOT NULL,
+    option_c VARCHAR(255) NOT NULL, 
+    option_d VARCHAR(255) NOT NULL,
+    bonne_reponse CHAR(1) NOT NULL  -- 'A', 'B', 'C', ou 'D
+);
+
+CREATE TABLE questions_utilisateur (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Identifiant unique pour chaque enregistrement dans la table
+    utilisateur_id INT NOT NULL,                  -- Identifiant de l'utilisateur qui a ajouté la question
+    question_id INT NOT NULL,                     -- Identifiant de la question ajoutée par l'utilisateur
+    date_ajout TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Date et heure d'ajout de la question, remplie automatiquement
+    FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id), -- Clé étrangère reliant utilisateur_id à l'id de la table utilisateur
+    FOREIGN KEY (question_id) REFERENCES questions(id)  -- Clé étrangère reliant question_id à l'id de la table questions
+);
