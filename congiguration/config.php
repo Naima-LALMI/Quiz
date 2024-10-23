2f29baecd0dd4baacbbf2f84093494ae35bb11ec
Naima <naima.lalmi28@gmail.com>
2024-10-23 09:37:05 +0200

    dossier config

---
+++ congiguration/config.php
@@ -0,0 +1,13 @@
+<?php
+// config.php
+
+// Paramètres de connexion à la base de données
+define('DB_HOST', 'localhost');  // Hôte de la base de données
+define('DB_NAME', 'quiz_assistante'); // Nom de la base de données
+define('DB_USER', 'ton_utilisateur'); // Utilisateur de la base de données
+define('DB_PASS', 'ton_mot_de_passe'); // Mot de passe de la base de données
+
+// Autres configurations éventuelles
+define('APP_NAME', 'Quiz Assistante Dentaire');
+?>
+``
