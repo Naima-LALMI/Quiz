f57764e23866d5883b0f49b1c426d8bbf371c70d
Naima <naima.lalmi28@gmail.com>
2024-10-25 13:36:57 +0200

    1 echo essai

--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
-<?php
-echo "Hello World!";
-?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+</head>
+<body>
+<h1>Quiz Assistantes dentaires </h1>
+<?php echo "Bienvenu  "?>
+
+  
+    
+</body>
+</html>
