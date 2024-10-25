f576873494b2f41cf6b776fbc5185b6480cdc0b8
Naima <naima.lalmi28@gmail.com>
2024-10-25 13:35:02 +0200

    parametre BDD ok

--- congiguration/config.php
+++ congiguration/config.php
@@ -3,11 +3,9 @@
 
 // Paramètres de connexion à la base de données
 define('DB_HOST', 'localhost');  // Hôte de la base de données
-define('DB_NAME', 'quiz_assistante'); // Nom de la base de données
-define('DB_USER', 'ton_utilisateur'); // Utilisateur de la base de données
-define('DB_PASS', 'ton_mot_de_passe'); // Mot de passe de la base de données
+define('DB_NAME', 'quiz'); // Nom de la base de données
+define('DB_USER', 'root'); // Utilisateur de la base de données
+define('DB_PASS', 'root'); // Mot de passe de la base de données
 
 // Autres configurations éventuelles
 define('APP_NAME', 'Quiz Assistante Dentaire');
-?>
-``
