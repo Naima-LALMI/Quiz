2e12155923d11b0591d869bf55bbb122fd002e7b
Naima <naima.lalmi28@gmail.com>
2024-10-26 18:45:49 +0200

    connection à la bdd ok

--- congiguration/config.php
+++ congiguration/config.php
@@ -1,11 +1,35 @@
 <?php
-// config.php
-
 // Paramètres de connexion à la base de données
+//Constantes d'environnement
 define('DB_HOST', 'localhost');  // Hôte de la base de données
-define('DB_NAME', 'quiz'); // Nom de la base de données
+define('DB_NAME', 'quiz_ad'); // Nom de la base de données
 define('DB_USER', 'root'); // Utilisateur de la base de données
-define('DB_PASS', 'root'); // Mot de passe de la base de données
+define('DB_PASS', ''); // Mot de passe de la base de données
 
 // Autres configurations éventuelles
 define('APP_NAME', 'Quiz Assistante Dentaire');
+
+// DSN de connexion pour PDO
+$dsn = "mysql:dbname=".DB_NAME.";host=".DB_HOST;
+
+// Se connecter à la BBD
+try{
+    // Une instance de PDO
+    $db = new PDO($dsn, DB_USER, DB_PASS);
+    echo"On est connectés ";
+
+    //envoyer les données en UTF8
+
+    $db->exec("SET NAMES utf8");
+// Execution que si souci avec le try
+}catch (PDOException $e){ 
+    die("erreur".$e->getMessage());
+}
+
+//Ici on est connectés à la base 
+// On peut recupere la liste des utilisateurs 
+$sql = "SELECT * FROM `utilisateur`";
+// On execute directement la requête
+$requete = $db->query($sql);
+var_dump($requete)
+?>
