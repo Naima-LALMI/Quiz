e3cb7ead8de0d8a4c3244d6a9904f63959a4c28d
Naima <naima.lalmi28@gmail.com>
2024-10-28 12:07:38 +0100

    requete CRUD

--- congiguration/config.php
+++ congiguration/config.php
@@ -10,26 +10,51 @@ define('DB_PASS', ''); // Mot de passe de la base de données
 define('APP_NAME', 'Quiz Assistante Dentaire');
 
 // DSN de connexion pour PDO
-$dsn = "mysql:dbname=".DB_NAME.";host=".DB_HOST;
+$dsn = "mysql:dbname=" . DB_NAME . ";host=" . DB_HOST;
 
 // Se connecter à la BBD
-try{
+try {
     // Une instance de PDO
     $db = new PDO($dsn, DB_USER, DB_PASS);
-    echo"On est connectés ";
+    echo "On est connectés ";
 
     //envoyer les données en UTF8
 
     $db->exec("SET NAMES utf8");
-// Execution que si souci avec le try
-}catch (PDOException $e){ 
-    die("erreur".$e->getMessage());
+
+    //On definit le mode de "fetch" par défaut
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    // Execution que si souci avec le try
+} catch (PDOException $e) {
+    die("erreur" . $e->getMessage());
 }
 
 //Ici on est connectés à la base 
+
 // On peut recupere la liste des utilisateurs 
 $sql = "SELECT * FROM `utilisateur`";
 // On execute directement la requête
 $requete = $db->query($sql);
-var_dump($requete)
-?>
+var_dump($requete);
+//On récupére les données (fetch ou fetchALl)
+$user = $requete->fetch();
+echo "<pre>";
+print_r($user);
+echo "</pre>";
+
+
+//Ajouter un utilisateur 
+// $sql = "INSERT INTO `utilisateur`(`nom`,`prenom`, `email`) VALUES ('Marin','Elisa','marin.elisa@exemple.com')";
+// $requete = $db->query($sql);
+// var_dump($requete);
+
+//modifier un utilisateur 
+$sql = "UPDATE `utilisateur` SET `email`='dupont.paul@exemple.com'  WHERE `id`=1";
+$requete = $db->query($sql);
+var_dump($requete);
+
+//supprimer un utilisateur
+$sql = "DELETE FROM `utilisateur` WHERE `id`=5";
+$requete = $db->query($sql);
+var_dump($requete);
